75aca6e8b4c087bbd0fd5f6dbf19c84f38e7204e
DJprogre33 <smirnovniki119@gmail.com>
2023-06-19 16:07:52 +0300

    comments and recommendations

--- web_blog/blog/views.py
+++ web_blog/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from  django.views.decorators.http import require_POST
+from django.db.models import Count
+from taggit.models import Tag
+
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, "blog/post/comment.html",
+                  {"post": post,
+                   "form": form,
+                   "comment": comment})
 
 
 def post_share(request, post_id):
@@ -44,8 +64,12 @@ class PostListView(ListView):
     template_name = "blog/post/list.html"
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     posts_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        posts_list = posts_list.filter(tags__in=[tag])
     # разбивка с 3 постами на страницу
     paginator = Paginator(posts_list, 3)
     page_number = request.GET.get("page", 1)
@@ -57,7 +81,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   "blog/post/list.html",
-                  {"posts": posts})
+                  {"posts": posts,
+                   "tag": tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -67,7 +92,19 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    # форма для комментариев
+    form = CommentForm()
+
+    # Список похожих постов
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by("-same_tags", "-publish")[:4]
 
     return render(request,
                   "blog/post/detail.html",
-                  {"post": post})
+                  {"post": post,
+                   "comments": comments,
+                   "form": form,
+                   "similar_posts": similar_posts})
+
