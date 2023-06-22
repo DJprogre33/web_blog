9aa374e6d69b7ee0c19d0c48f7b98b91276522c3
DJprogre33 <smirnovniki119@gmail.com>
2023-06-22 14:07:36 +0300

    migrations_finished

--- web_blog/config/settings.py
+++ web_blog/config/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     "taggit",
     "django.contrib.sites",
-    "django.contrib.sitemaps"
+    "django.contrib.sitemaps",
+    "django.contrib.postgres"
 ]
 
 MIDDLEWARE = [
@@ -81,17 +82,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv("PG_NAME"),
-#         'USER': os.getenv("PG_USER"),
-#         'PASSWORD': os.getenv("PG_PASSWORD"),
-#         # 'HOST': "localhost",
-#         # 'PORT': "5432"
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -102,6 +92,13 @@ DATABASES = {
         # 'PORT': "5432"
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
